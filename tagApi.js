9cbe22fd9fc9f24fc4a7e99ad3fa250652000f32
atupal <atupalykl@gmail.com>
2015-07-28 07:25:50 +0000

    update api

--- tagApi.js
+++ tagApi.js
@@ -1,30 +1,71 @@
 
 var unirest = require("unirest");
-var req = unirest("GET", "http://api.imagga.com/v1/tagging");
 var fs = require("fs");
 
-function uploadImage(filepath, callback) {
-  unirest.post("http://api.imagga.com/v1/content")
-          .header({
-            //"authorization": "Basic YWNjXzJkYzdkNzNjMmYwODliMToxYzQ3Yzg2ZDg0YjdmYjdjYjZjNzQ1NTQ1MmYwNTgzMQ==",
-            "authorization": "Basic YWNjXzM3MGI3MWVkNWE5MzA1ODplNjg3ZmRiMTkzMzBiZmFjMTcyNGRiNjhiMjkyN2IzMA==",
-            "accept": "application/json"
-          })
-          .attach('file', filepath)
-          .end(function (response) {
-            //console.log(response.body);
-            /*
-             * { status: 'success',
-             *   uploaded:
-             *      [ { id: 'ae309b386efbcf7fb8d40431d8c470c6',
-             *             filename: 'example_photo.jpg' } ] }
-             */
-            callback(response.body.uploaded[0].id);
-            //getImageTags(response.body.uploaded[0].id);
-          });
+function uploadImage(filepaths, callback) {
+
+  var headers = {
+    //"authorization": "Basic YWNjXzJkYzdkNzNjMmYwODliMToxYzQ3Yzg2ZDg0YjdmYjdjYjZjNzQ1NTQ1MmYwNTgzMQ==",
+    "authorization": "Basic YWNjXzM3MGI3MWVkNWE5MzA1ODplNjg3ZmRiMTkzMzBiZmFjMTcyNGRiNjhiMjkyN2IzMA==",
+    "accept": "application/json"
+  }
+
+  var req = unirest.post("http://api.imagga.com/v1/content").header(headers);
+  for (var idx in filepaths) {
+    req.attach('file'+idx, filepaths[idx]);
+  }
+
+  req.end(function (response) {
+
+    //console.log(response.body);
+
+    /*
+     * { status: 'success',
+     *   uploaded:
+     *      [ { id: 'ae309b386efbcf7fb8d40431d8c470c6',
+     *             filename: 'example_photo.jpg' } ] }
+     */
+
+    var ids = {};
+    for (var idx in response.body.uploaded) {
+      ids[response.body.uploaded[idx].id] = response.body.uploaded[idx].filename;
+    }
+
+    var req = unirest("GET", "http://api.imagga.com/v1/tagging").header(headers);
+    req.query({
+      "version": "2"
+    });
+
+    for (var id in ids) {
+      req.query({
+        "content": id
+      })
+    }
+
+    req.end(function(response) {
+      var results = {};
+      for (var idx in response.body.results) {
+        var tagsDict = response.body.results[idx].tags;
+        var tags = [];
+        var cnt = 0;
+        for (var i in tagsDict) {
+          tags.push(tagsDict[i].tag);
+          cnt += 1;
+          if (cnt >= 10) break;
+        }
+
+        results[ ids[response.body.results[idx].image] ] = tags;
+      }
+
+      callback(results);
+    });
+
+  });
+
 }
 
 function getImageTags(contentId) {
+  var req = unirest("GET", "http://api.imagga.com/v1/tagging");
 
   //var fileContent = fs.readFileSync(filepath);
 
@@ -49,6 +90,20 @@ function getImageTags(contentId) {
 
 }
 
-uploadImage("./example_photo.jpg", getImageTags);
-//uploadImage("/home/atupal/Pictures/black.png")
-//getImageTags("ae309b386efbcf7fb8d40431d8c470c6");
+
+function analyzeImage(filepath, callback) {
+  unirest.post("https://api.projectoxford.ai/vision/v1/analyses?visualFeatures=Faces")
+  //unirest.post("https://api.projectoxford.ai/vision/v1/analyses?visualFeatures=All")
+    .header({
+      "Content-Type": "multipart/form-data",
+      "Ocp-Apim-Subscription-Key": "38762bee7d8440ae91409f5ee5a1f396",
+    })
+    .attach('file', filepath)
+    .end(function(response){
+      callback(response.body);
+    });
+}
+
+//uploadImage(["./example_photo.jpg", "./black.png"], console.log);
+//analyzeImage("./fbb.jpg", console.log);
+analyzeImage("./example_photo.jpg", console.log);
